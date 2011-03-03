21ed6d508e9ab126cd83ac86e3e0d66536592162
SplinterOfChaos <hakusa@gmail.com>
2011-03-03 12:10:13 -0500

    Nicer colors.

--- main.cpp
+++ main.cpp
@@ -151,10 +151,10 @@ class Platform : public Square
         };
 
         Vec3 wall[] = {
-            Vec3( -scale, -scale,   z ),
+            Vec3( -scale, -scale,   z  ),
             Vec3( -scale, -scale, -100 ),
             Vec3( -scale,  scale, -100 ),
-            Vec3( -scale,  scale,   z )
+            Vec3( -scale,  scale,   z  )
         };
 
         draw::Verts< Vec3 > verts( square, 4 );
@@ -163,15 +163,21 @@ class Platform : public Square
         glColor3f( r, g, b );
         draw::draw( verts );
 
-        float intensity = 1;//( r + g + b ) / 3;
-        glColor3f( 0.4*intensity, 8.4*intensity, 0.1*intensity );
+        float intensity = ( r + g + b ) / 3;
+        intensity *= intensity;
 
-    
+        glColor3f( 0.8*intensity, 0.4*intensity, 0.1*intensity );
         draw::draw( side );
+
+        glRotatef( 90, 0, 0, 1 );
+        glColor3f( 0.85*intensity, 0.5*intensity, 0.15*intensity );
+        draw::draw( side );
+
+        glColor3f( 0.9*intensity, 0.6*intensity, 0.15*intensity );
         glRotatef( 90, 0, 0, 1 );
         draw::draw( side );
-        glRotatef( 90, 0, 0, 1 );
-        draw::draw( side );
+
+        glColor3f( 0.95*intensity, 0.7*intensity, 0.2*intensity );
         glRotatef( 90, 0, 0, 1 );
         draw::draw( side );
 
