95c607a6d9aa0a6166a14993b88efa108fc24a5b
SplinterOfChaos <hakusa@gmail.com>
2011-03-03 22:36:24 -0500

    Moved some code into Screen.h/cpp.

--- main.cpp
+++ main.cpp
@@ -29,73 +29,7 @@ bool showFrameTime = false;
 
 float zRot = 0;
 
-bool resize_window( float w, float h )
-{
-    if( !SDL_SetVideoMode(w, h, 32, SDL_OPENGL|SDL_RESIZABLE) )
-        return false;
-
-    glViewport( 0, 0, w, h );
-    glMatrixMode( GL_PROJECTION );
-    glLoadIdentity();
-    glOrtho( -w/2, w/2, h/2, -h/2, -1000, 1000 );
-    glMatrixMode( GL_MODELVIEW );
-
-    return true;
-}
-
-GLenum init_gl( int w, int h )
-{
-    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
-
-    resize_window( w, h );
-
-    glEnable( GL_BLEND );
-    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-
-    glDepthFunc( GL_LEQUAL );
-    glEnable( GL_DEPTH_TEST );
-
-    return glGetError();
-}
-
-#ifdef __WIN32
-// Borrowed and modified from http://www.devmaster.net/forums/showthread.php?t=443
-void set_vsync( int interval = 1 )
-{
-    typedef BOOL (APIENTRY *PFNWGLSWAPINTERVALFARPROC)( int );
-    PFNWGLSWAPINTERVALFARPROC wglSwapIntervalEXT = 0;
-
-    const char *extensions = (const char*)glGetString( GL_EXTENSIONS );
-
-    if( strstr( extensions, "WGL_EXT_swap_control" ) == 0 ) {
-        return; // Error: WGL_EXT_swap_control extension not supported on your computer.\n");
-    } else {
-        wglSwapIntervalEXT = (PFNWGLSWAPINTERVALFARPROC)wglGetProcAddress( "wglSwapIntervalEXT" );
-
-        if( wglSwapIntervalEXT )
-            wglSwapIntervalEXT( interval );
-    }
-}
-#endif
-
-bool make_sdl_gl_window( int w, int h )
-{
-    if( ! resize_window(w,h) )
-        return false;
-    init_gl( w, h );
-
-#ifdef __WIN32
-    set_vsync( 0 );
-#endif
-
-    return true;
-}
-
-void update_screen()
-{
-    SDL_GL_SwapBuffers();
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-}
+#include "Screen.h"
 
 void keyboard_events()
 {
