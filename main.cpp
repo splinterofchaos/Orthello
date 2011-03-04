89c753d22091aa3b0c708e4e1947a19b0831a796
SplinterOfChaos <hakusa@gmail.com>
2011-03-03 22:29:46 -0500

    Moved Platform code to Platform.h/cpp.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
  
 // Local includes.
 #include "Actor.h"
+#include "Platform.h"
 
 #include "Draw.h"
 
@@ -99,79 +100,6 @@ void update_screen()
 void keyboard_events()
 {
 }
-
-class Platform : public Square
-{
-    float r, g, b;
-
-  public:
-    typedef Vector<float,2> Vec;
-
-    int growthLeft;
-
-    Platform( const Vec& pos )
-    {
-        s = pos;
-        scale = 0;
-
-        growthLeft = 50;
-
-        r = random( 0.2f, 0.4f );
-        g = random( 0.7f, 1.0f );
-        b = random( 0.4f, 0.5f );
-    }
-
-    void draw()
-    {
-        float z = scale / 2;
-
-        glPushMatrix();
-        glTranslatef( s.x(), s.y(), z );
-        
-        typedef Vector<float,3> Vec3;
-
-        Vec3 square[] = {
-            Vec3( -scale, -scale, z ),
-            Vec3(  scale, -scale, z ),
-            Vec3(  scale,  scale, z ),
-            Vec3( -scale,  scale, z )
-        };
-
-        Vec3 wall[] = {
-            Vec3( -scale, -scale,   z  ),
-            Vec3( -scale, -scale, -100 ),
-            Vec3( -scale,  scale, -100 ),
-            Vec3( -scale,  scale,   z  )
-        };
-
-        draw::Verts< Vec3 > verts( square, 4 );
-        draw::Verts< Vec3 > side( wall, 4 );
-
-        glColor3f( r, g, b );
-        draw::draw( verts );
-
-        float intensity = ( r + g + b ) / 3;
-        intensity *= intensity;
-
-        glColor3f( 0.8*intensity, 0.4*intensity, 0.1*intensity );
-        draw::draw( side );
-
-        glRotatef( 90, 0, 0, 1 );
-        glColor3f( 0.85*intensity, 0.5*intensity, 0.15*intensity );
-        draw::draw( side );
-
-        glColor3f( 0.9*intensity, 0.6*intensity, 0.15*intensity );
-        glRotatef( 90, 0, 0, 1 );
-        draw::draw( side );
-
-        glColor3f( 0.95*intensity, 0.7*intensity, 0.2*intensity );
-        glRotatef( 90, 0, 0, 1 );
-        draw::draw( side );
-
-        glPopMatrix();
-    }
-
-};
 
 typedef std::vector< Platform > Platforms;
 Platforms platforms;
