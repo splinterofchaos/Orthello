9d9709b5930ea6c3467ce68af4caea1e93ad45d5
splinterofchaos <hakusa@gmail.com>
2011-04-23 03:45:44 -0400

    Added decent normals to Plat $ they now use circular collision.

--- Platform.cpp
+++ Platform.cpp
@@ -57,14 +57,23 @@ void Platform::draw()
     const float SLOPE_H = 0.6;
     const float SLOPE_W = 0.5;
 
-    Vector<float,3> circle[ CIRCLE_SIZE + 2 ];
-    circle[0] = vector( 0, 0, z );
+    Vector<float,3> circleT[ CIRCLE_SIZE ];
     for( size_t i=0; i < CIRCLE_SIZE; i++ )
     {
-        circle[i+1].x() = unitCircle[i].x() * scale;
-        circle[i+1].y() = unitCircle[i].y() * scale;
-        circle[i+1].z( z * SLOPE_H );
+        circleTop[i] = vector(
+            unitCircle[i].x(), unitCircle[i].y(), z 
+        );
     }
+
+    Vector<float,3> circle[ CIRCLE_SIZE * 2 + 2 ];
+    for( size_t i=0; i < CIRCLE_SIZE; i++ )
+    {
+        circle[i*2+1].x() circle[i*2] = unitCircle[i].x();
+        circle[i*2+1].y() circle[i*2] = unitCircle[i].y();
+        circle[i*2] = z;
+        circle[i*2+1].z( z * SLOPE_H );
+    }
+    circle[ CIRCLE_SIZE   ] = circle[0];
     circle[ CIRCLE_SIZE+1 ] = circle[1];
 
     Vector<float,3> sideV[ CIRCLE_SIZE * 2 ];
@@ -82,6 +91,7 @@ void Platform::draw()
         wallN[i*2].z() = wallN[i*2+1].z() = 0;
     }
 
+    draw::Verts< Vec3 > veryTop( circleT, CIRCLE_SIZE );
     draw::Verts< Vec3 > top( circle, CIRCLE_SIZE+2 );
 
     Vec3 topNorms[] = {
@@ -138,9 +148,12 @@ void Platform::draw()
     glEnableClientState( GL_NORMAL_ARRAY );
 
     glColor3f( r + lightAdd, g + lightAdd, b + lightAdd );
-    glNormalPointer( GL_FLOAT, 0, circle );
 
-    draw::draw( top, GL_POLYGON );
+    glNormalPointer( GL_FLOAT, 0, circleT );
+    draw::draw( veryTop, GL_POLYGON );
+
+    glNormalPointer( GL_FLOAT, 0, circle );
+    draw::draw( top, GL_TRIANGLE_STRIP );
 
     float intensity = ( r + g + b ) / 3;
     intensity *= 2 * intensity;
